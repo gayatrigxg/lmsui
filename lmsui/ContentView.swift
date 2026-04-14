317a8ab7b2f1849ee300122542f5b444f6a4f0cc
gayatri <gayatrigoundadkar@gmail.com>
2026-04-14 15:43:03 +0530

    Initial Commit

---
+++ lmsui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lmsui
+//
+//  Created by Gayatri Goundadkar on 14/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lmsui/lmsuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lmsuiApp.swift
+//  lmsui
+//
+//  Created by Gayatri Goundadkar on 14/04/26.
+//
+
+import SwiftUI
+
+@main
+struct lmsuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
